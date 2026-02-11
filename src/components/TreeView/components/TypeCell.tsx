da0a05faefca5e5931c61f03e7de70b425e632fc
Paul Chasle <1671213+pchasle@users.noreply.github.com>
2026-02-11 20:07:10 +0100

    improve collapse arrow

--- src/components/TreeView/components/TypeCell.tsx
+++ src/components/TreeView/components/TypeCell.tsx
@@ -1,6 +1,12 @@
-import { ArrowDownIcon, ArrowRightIcon, Tag } from "akeneo-design-system";
+import {
+  ArrowDownIcon,
+  ArrowRightIcon,
+  IconButton,
+  Tag,
+} from "akeneo-design-system";
 import type { ProductType } from "../types.ts";
 import styled from "styled-components";
+import { useTranslate } from "@akeneo-pim/shared";
 
 export const TypeCell = ({
   type,
@@ -13,6 +19,8 @@ export const TypeCell = ({
   hasChildren?: boolean;
   onToggle?: () => void;
 }) => {
+  const translate = useTranslate();
+
   switch (type) {
     case "model":
       return (
@@ -23,18 +31,37 @@ export const TypeCell = ({
     case "submodel":
       return (
         <TypeCellWrapper $indent={0}>
-          {hasChildren && (
+          {hasChildren ? (
             <>
-              <span
-                onClick={(e) => {
-                  e.stopPropagation();
-                  onToggle?.();
-                }}
-              >
-                {isCollapsed ? <ArrowRightIcon /> : <ArrowDownIcon />}
-              </span>{" "}
+              {isCollapsed ? (
+                <IconButton
+                  icon={<ArrowRightIcon />}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onToggle?.();
+                  }}
+                  title={translate("Expand")}
+                  size="small"
+                  ghost="borderless"
+                  level="tertiary"
+                />
+              ) : (
+                <IconButton
+                  icon={<ArrowDownIcon />}
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    onToggle?.();
+                  }}
+                  title={translate("Collapse")}
+                  size="small"
+                  ghost="borderless"
+                  level="tertiary"
+                />
+              )}
             </>
-          )}
+          ) : (
+            <ArrowPlaceholder />
+          )}{" "}
           <Tag tint="dark_cyan">{type}</Tag>
         </TypeCellWrapper>
       );
@@ -50,4 +77,9 @@ export const TypeCell = ({
 const TypeCellWrapper = styled.span<{ $indent: number }>`
   padding-left: ${({ $indent }) => $indent}px;
   display: inline-block;
+`;
+
+const ArrowPlaceholder = styled.span`
+  display: inline-block;
+  width: 24px;
 `;
