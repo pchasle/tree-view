ef58e79a9f8dc3a4fff9244a5a01d78c79e89253
Paul Chasle <1671213+pchasle@users.noreply.github.com>
2026-02-11 11:13:42 +0100

    setup react 17 + dsm

---
+++ src/components/TreeView.tsx
@@ -0,0 +1,13 @@
+import { Table } from "akeneo-design-system";
+
+export const TreeView = () => {
+  return (
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          <Table.HeaderCell>Header 1</Table.HeaderCell>
+        </Table.Row>
+      </Table.Header>
+    </Table>
+  );
+};
